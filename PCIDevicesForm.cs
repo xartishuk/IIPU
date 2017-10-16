e6cab55be3f12dc7d0fb4b6b517ba8188348ab52
xartishuk <xartishuk@gmail.com>
2017-10-16 16:11:37 +0300

    Add files via upload

---
+++ PCIDevicesForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LabIIPU_PCI
+{
+    public partial class PCIDevicesList : Form
+    {
+        public PCIDevicesList()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+
+        private string GetProductName(List<string> transcriptOfPIDs, List<string> PIDs)
+        {
+            foreach (String nameOfProduct in transcriptOfPIDs)
+            {
+                if (nameOfProduct.Contains(PIDs[0].ToLower()))
+                {
+                    PIDs.RemoveAt(0);
+                    return nameOfProduct;
+                }
+            }
+            return null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //PCIDevices.DownloadFileOfDevIDs();
+
+            var transcriptOfVIDs = PCIDevices.ParseVIDsTranscript();
+            var transcriptOfPIDs = PCIDevices.ParsePIDsTranscript();
+            var VIDs = new List<string>();
+            var PIDs = new List<string>();
+            var DevicesOnPci = PCIDevices.GetDevicesID("Win32_PnPEntity", "PnPDeviceID");
+
+            foreach (string device in DevicesOnPci)
+            {
+                VIDs.Add(PCIDevices.ParseVidFromDeviceID(device));
+                PIDs.Add(PCIDevices.ParsePidFromDeviceID(device));
+            }
+
+            for(int i = 0; i < transcriptOfVIDs.Count; i++)
+            { 
+                if (VIDs.Count > 0 && transcriptOfVIDs[i].Contains(VIDs[0].ToLower()))
+                {
+                    textBox1.Text = textBox1.Text + "VID: " + transcriptOfVIDs[i] + Environment.NewLine + "PID: "
+                        + GetProductName(transcriptOfPIDs, PIDs).Trim() + Environment.NewLine + Environment.NewLine;
+                    VIDs.RemoveAt(0);
+                    i = 0;
+                }
+            }
+        }
+    }
+}
